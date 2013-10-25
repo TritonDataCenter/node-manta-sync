a514f7e84debca6ca820cb5a3edbf291d17e7b2d
Dave Eddy <dave@daveeddy.com>
2013-10-25 19:03:10 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,190 @@
+manta-sync
+==========
+
+Rsync style command for [Joyent's Manta](http://www.joyent.com/products/manta)
+
+Installation
+------------
+
+    npm install -g manta-sync
+
+Usage
+-----
+
+    manta-sync ./ ~~/stor/foo
+
+`manta-sync` requires 2 arguments, the first is a local directory that you
+would like to sync *the contents* of into manta.  The second is
+a manta directory that you would like the files to by synced to.
+
+All remote directories will be lazily created for you if they do not exist,
+relying on the latest `manta` node module for this behavior.
+
+`manta-sync` has slightly different usage than the standard node manta
+tools, it requires `MANTA_USER`, `MANTA_URL` and `MANTA_KEY_ID` be set.
+
+    usage: manta-sync [options] localdir ~~/remotedir
+
+    synchronize all files found inside `localdir` to `~~/remotedir`
+
+    examples
+      manta-sync ./ ~~/stor/foo
+        -- sync all files in your cwd to the dir ~~/stor/foo
+      manta-sync --dry-run ./ ~~/stor/foo
+        -- same as above, but just HEAD the data, don't PUT
+
+    options
+      -c, --concurrency <num>   max number of parallel HEAD's or PUT's to do, defaults to 30
+      -h, --help                print this message and exit
+      -m, --md5                 use md5 instead of file size (slower, but more accurate)
+      -n, --dry-run             do everything except PUT any files
+      -u, --updates             check for available updates on npm
+      -v, --version             print the version number and exit
+
+Example
+-------
+
+First we'll create a basic directory structure we want to sync to manta
+
+    $ mkdir foo
+    $ touch foo/a foo/b foo/c
+    $ mkdir foo/d
+    $ touch foo/d/e
+    $ ls foo/
+    a  b  c  d/
+    $ ls foo/d
+    e
+
+Now, let's look at the remote end to see what we're dealing with
+
+    $ mls ~~/stor
+    $
+
+Nothing on the remote end yet, let's sync the files up
+
+    $ manta-sync foo/ ~~/stor/foo
+    building local file list...
+    local file list built, 4 files found
+
+    ~~/stor/foo/d/e... not found, adding to put list (1/4)
+    ~~/stor/foo/c... not found, adding to put list (2/4)
+    ~~/stor/foo/b... not found, adding to put list (3/4)
+    ~~/stor/foo/a... not found, adding to put list (4/4)
+
+    upload list built, 4 files staged for uploading
+
+    ~~/stor/foo/a... uploaded (1/4)
+    ~~/stor/foo/b... uploaded (2/4)
+    ~~/stor/foo/c... uploaded (3/4)
+    ~~/stor/foo/d/e... uploaded (4/4)
+
+    4 files put successfully, 0 files failed to put
+
+    done
+
+All 4 files were uploaded (and their directories created), we can verify this with
+
+    $ mls ~~/stor
+    foo/
+    $ mls ~~/stor/foo
+    a
+    b
+    c
+    d/
+    $ mls ~~/stor/foo/d
+    e
+
+Now that we are synced up, let's run it again and see what happens
+
+    $ manta-sync foo/ ~~/stor/foo
+    building local file list...
+    local file list built, 4 files found
+
+    ~~/stor/foo/b... size same as local file, skipping (1/4)
+    ~~/stor/foo/d/e... size same as local file, skipping (2/4)
+    ~~/stor/foo/c... size same as local file, skipping (3/4)
+    ~~/stor/foo/a... size same as local file, skipping (4/4)
+
+    upload list built, 0 files staged for uploading
+
+
+    done
+
+This time the output is slightly different, because the files were
+found on the remote end and the have the same size as the local files.
+
+So let's modify a file and rerun the sync
+
+    $ echo hello > foo/a
+    $ manta-sync foo/ ~~/stor/foo
+    building local file list...
+    local file list built, 4 files found
+
+    ~~/stor/foo/c... size same as local file, skipping (1/4)
+    ~~/stor/foo/a... size is different, adding to put list (2/4)
+    ~~/stor/foo/d/e... size same as local file, skipping (3/4)
+    ~~/stor/foo/b... size same as local file, skipping (4/4)
+
+    upload list built, 1 files staged for uploading
+
+    ~~/stor/foo/a... uploaded (1/1)
+
+    1 files put successfully, 0 files failed to put
+
+    done
+
+`manta-sync` detected one of the files on the local end was a different
+size than reported by manta, so it staged it for uploading, and `PUT`
+the file.
+
+How
+---
+
+`manta-sync` works in 4 stages
+
+### 1. Find all local files
+
+The node module [findit](https://github.com/substack/node-findit) is used to
+locate (and `stat(2)`) all local files, to build a list of files that need
+to be synced.
+
+### 2. Process each local file, figure out if we need to put a new version into Manta
+
+For each local file found, a corresponding remote manta filename is constructed, and
+then checked for info (`HEAD` request) to see if it exists, and what its size is if
+it is found.
+
+If the file is not found (`404` / `NotFoundError`) it is staged for uploading.
+
+If the file is found, and the size reported by manta is different than the size
+on the filesystem, it is also staged for uploading.  This behavior can be
+modified with the `-m` or `--md5` switch, which tells `manta-sync` to use the md5 hash
+of a file instead of the file size.
+
+### 3. Upload each file that needs to be uploaded, lazily handling directory creation
+
+For each file that has been staged for uploading, a `PUT` request is made, and
+all directories that are needed are created lazily (which may result in more than
+1 `PUT` per file).
+
+If `-n` or `--dry-run` is supplied, this step is skipped  by just printing
+what actions would have been taking.  Note that during a dry-run, `HEAD` requests
+are still made.
+
+### 4. Print statistics, clean up
+
+`manta-sync` prints how many files were uploaded, and how many (if any) files failed
+to upload.  Also, any errors that were encountered are displayed again at the bottom of
+the output.
+
+Possible Future Features
+------------------------
+
+- Remote => Local sync
+- identify files on the remote end not found on the local end (and unlink them)
+- count number of `HEAD` and `PUT` requests done (for billing purposes)
+
+License
+-------
+
+MIT
---
+++ manta-sync.js
@@ -0,0 +1,289 @@
+#!/usr/bin/env node
+/**
+ * Rsync style command for Joyent's Manta
+ *
+ * Author: Dave Eddy <dave@daveeddy.com>
+ * Date: 10/24/13
+ * License: MIT
+ */
+
+var crypto = require('crypto');
+var fs = require('fs');
+var path = require('path');
+var util = require('util');
+
+var async = require('async');
+var findit = require('findit');
+var getopt = require('posix-getopt');
+var manta = require('manta');
+
+// XXX https://github.com/joyent/node-manta/issues/139
+var EMPTY_MD5 = 'd41d8cd98f00b204e9800998ecf8427e';
+
+var package = require('./package.json');
+
+function usage() {
+  return [
+    'usage: manta-sync [options] localdir ~~/remotedir',
+    '',
+    'synchronize all files found inside `localdir` to `~~/remotedir`',
+    '',
+    'examples',
+    '  manta-sync ./ ~~/stor/foo',
+    '    -- sync all files in your cwd to the dir ~~/stor/foo',
+    '  manta-sync --dry-run ./ ~~/stor/foo',
+    '    -- same as above, but just HEAD the data, don\'t PUT',
+    '',
+    'options',
+    '  -c, --concurrency <num>   max number of parallel HEAD\'s or PUT\'s to do, defaults to ' + opts.concurrency,
+    '  -h, --help                print this message and exit',
+    '  -m, --md5                 use md5 instead of file size (slower, but more accurate)',
+    '  -n, --dry-run             do everything except PUT any files',
+    '  -u, --updates             check for available updates on npm',
+    '  -v, --version             print the version number and exit'
+  ].join('\n');
+}
+
+// command line arguments
+var options = [
+  'c:(concurrency)',
+  'h(help)',
+  'm(md5)',
+  'n(dry-run)',
+  'u(updates)',
+  'v(version)'
+].join('');
+var parser = new getopt.BasicParser(options, process.argv);
+
+var opts = {
+  concurrency: 30,
+  dryrun: false,
+  md5: false
+};
+var option;
+while ((option = parser.getopt()) !== undefined) {
+  switch (option.option) {
+    case 'c': opts.concurrency = +option.optarg; break;
+    case 'm': opts.md5 = true; break;
+    case 'h': console.log(usage()); process.exit(0);
+    case 'n': opts.dryrun = true; break;
+    case 'u': // check for updates
+      require('latest').checkupdate(package, function(ret, msg) {
+        console.log(msg);
+        process.exit(ret);
+      });
+      return;
+    case 'v': console.log(package.version); process.exit(0);
+    default: console.error(usage()); process.exit(1); break;
+  }
+}
+var args = process.argv.slice(parser.optind());
+
+if (args.length !== 2) {
+  console.error('[error] must supply exactly 2 operands\n');
+  console.error(usage());
+  process.exit(1);
+}
+if (!process.env.MANTA_KEY_ID ||
+    !process.env.MANTA_USER   ||
+    !process.env.MANTA_URL) {
+  console.error('environmental variables MANTA_USER, MANTA_URL, and MANTA_KEY_ID must be set');
+  console.error(usage());
+  process.exit(1);
+}
+
+var localdir = path.resolve(args[0]);
+var remotedir = args[1];
+
+var client = manta.createClient({
+  sign: manta.sshAgentSigner({
+    keyId: process.env.MANTA_KEY_ID,
+    user: process.env.MANTA_USER
+  }),
+  user: process.env.MANTA_USER,
+  url: process.env.MANTA_URL
+});
+
+var finder = findit(localdir);
+
+// 1. Find all local files
+if (opts.dryrun)
+  console.log('== dryrun ==');
+console.log('building local file list...');
+var localfiles = [];
+var infoqueue = async.queue(processfile, opts.concurrency);
+finder.on('file', function(file, stat) {
+  if (file.indexOf(localdir) !== 0)
+    return console.error('error processing %s', file);
+
+  localfiles.push({file: file, stat: stat});
+});
+
+finder.on('end', function() {
+  console.log('local file list built, %d files found\n', localfiles.length);
+  if (!localfiles.length)
+    return done();
+  localfiles.forEach(function(localfile) {
+    infoqueue.push(localfile, function() {});
+  });
+});
+
+// 2. Process each local file, figure out if we need to put
+// a new version into Manta
+var processed = 0;
+var filestoput = [];
+var errors = [];
+var putqueue = async.queue(putfile, opts.concurrency);
+function processfile(d, cb) {
+  /**
+   * $ manta-sync ./foo ~~/stor/foo
+   * d.file = /home/dave/foo/file.txt (absolute path)
+   * d.stat = [Stat object]
+   * d.basefile = file.txt
+   * d.mantafile = ~~/store/foo/file.txt
+   */
+
+  d.basefile = d.file.substr(localdir.length);
+  d.mantafile = path.join(remotedir, d.basefile);
+
+  client.info(d.mantafile, function(err, info) {
+    if (err) {
+      processed++;
+      if (err.code === 'NotFoundError') {
+        console.log('%s... not found, adding to put list (%d/%d)',
+            d.mantafile, processed, localfiles.length);
+        filestoput.push(d);
+      } else {
+        var s = util.format('%s... unknown error: %s (%d/%d)',
+            d.mantafile, err.code, processed, localfiles.length);
+        console.error(s);
+        errors.push(s);
+      }
+      cb();
+      return;
+    }
+
+    if (opts.md5) {
+      // md5
+      var md5sum = crypto.createHash('md5');
+      var rs = fs.createReadStream(d.file);
+      rs.on('error', function(err) {
+        processed++;
+        var s = util.format('%s... read error: %s (%d/%d)',
+            d.mantafile, err.message, processed, localfiles.length);
+        console.error(s);
+        errors.push(s);
+      });
+
+      rs.on('data', md5sum.update.bind(md5sum));
+      rs.on('end', function() {
+        processed++;
+        var md5 = md5sum.digest('hex');
+
+        var remotemd5 = info.md5;
+        if (!remotemd5) {
+          remotemd5 = EMPTY_MD5;
+        } else {
+          remotemd5 = new Buffer(remotemd5, 'base64').toString('hex');
+        }
+
+        if (md5 === remotemd5) {
+          console.log('%s... md5 same as local file, skipping (%d/%d)',
+            d.mantafile, processed, localfiles.length);
+        } else {
+          console.log('%s... md5 is different, adding to put list (%d/%d)',
+            d.mantafile, processed, localfiles.length);
+          filestoput.push(d);
+        }
+
+        cb();
+      });
+      return;
+    } else {
+      processed++;
+      // XXX +info.size is a hack
+      // https//github.com/joyent/node-manta/issues/136
+      if (d.stat.size === +info.size) {
+        console.log('%s... size same as local file, skipping (%d/%d)',
+            d.mantafile, processed, localfiles.length);
+      } else {
+        console.log('%s... size is different, adding to put list (%d/%d)',
+            d.mantafile, processed, localfiles.length);
+        filestoput.push(d);
+      }
+      cb();
+      return;
+    }
+  });
+}
+
+infoqueue.drain = function() {
+  processed = 0;
+  console.log('\nupload list built, %d files staged for uploading\n',
+      filestoput.length);
+  if (!filestoput.length)
+    return done();
+  filestoput.forEach(function(filetoput) {
+    putqueue.push(filetoput, function() {});
+  });
+};
+
+// 3. Upload each file that needs to be uploaded, lazily handling
+// directory creation
+var filesput = 0;
+var filesnotput = 0;
+function putfile(d, cb) {
+  if (opts.dryrun) {
+    console.log('%s... uploaded (dryrun)', d.mantafile);
+    return cb();
+  }
+  var rs = fs.createReadStream(d.file);
+  rs.on('error', function(err) {
+    processed++;
+    var s = util.format('%s... error opening file: %s (%d/%d)',
+      d.mantafile, err.message, processed, filestoput.length);
+    console.error(s);
+    errors.push(s);
+    filesnotput++;
+    cb();
+  });
+
+  rs.on('open', function(fd) {
+    client.put(d.mantafile, rs, {size: d.stat.size, mkdirs: true}, function(err) {
+      processed++;
+      if (err) {
+        var s = util.format('%s... error uploading: %s (%d/%d)',
+          d.mantafile, err.code, processed, filestoput.length);
+        console.error(s);
+        errors.push(s);
+        filesnotput++;
+      } else {
+        console.log('%s... uploaded (%d/%d)',
+          d.mantafile, processed, filestoput.length);
+        filesput++;
+      }
+      cb();
+    });
+  });
+}
+
+putqueue.drain = function() {
+  console.log('\n%d files put successfully, %d files failed to put',
+      filesput, filesnotput);
+  done();
+};
+
+// 4. Done
+function done() {
+  var ret = 0;
+  if (errors.length) {
+    ret = 1;
+    console.error('\n== errors\n');
+    errors.forEach(function(error) {
+      console.error(error);
+    });
+  }
+  console.log('\ndone');
+  client.close();
+  process.exit(ret);
+}
