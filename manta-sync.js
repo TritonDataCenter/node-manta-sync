ed96de5f52d6d28a597eefb7aaac3685f00dbd5d
isaacs <i@izs.me>
2013-11-23 14:11:15 -0800

    Refactor to use Cuttlefish

--- manta-sync.js
+++ manta-sync.js
@@ -16,11 +16,11 @@ var async = require('async');
 var findit = require('findit');
 var getopt = require('posix-getopt');
 var manta = require('manta');
-
-// XXX https://github.com/joyent/node-manta/issues/139
-var EMPTY_MD5 = 'd41d8cd98f00b204e9800998ecf8427e';
+var cuttlefish = require('cuttlefish');
 
 var package = require('./package.json');
+
+var errors = [];
 
 function usage() {
   return [
@@ -32,7 +32,7 @@ function usage() {
     '  manta-sync ./ ~~/stor/foo',
     '    -- sync all files in your cwd to the dir ~~/stor/foo',
     '  manta-sync --dry-run ./ ~~/stor/foo',
-    '    -- same as above, but just HEAD the data, don\'t PUT',
+    '    -- same as above, but just HEAD the data, don\'t PUT or DELETE',
     '',
     'options',
     '  -c, --concurrency <num>   max number of parallel HEAD\'s or PUT\'s to do, defaults to ' + opts.concurrency,
@@ -123,283 +123,117 @@ var finder = findit(localdir);
 if (opts.dryrun)
   console.log('== dryrun ==');
 console.log('building local file list...');
-var localfiles = [];
+
+var files = {}
+var localFound = 0
 finder.on('file', function(file, stat) {
   if (file.indexOf(localdir) !== 0)
     return console.error('error processing %s', file);
 
-  /**
-   * $ manta-sync ./foo ~~/stor/foo
-   * d.file = /home/dave/foo/file.txt (absolute path)
-   * d.stat = [Stat object]
-   * d.basefile = file.txt
-   * d.mantafile = ~~/store/foo/file.txt
-   */
-  var d = {
-    file: file,
-    stat: stat,
-    basefile: file.substr(localdir.length)
-  };
-  d.mantafile = path.join(remotedir, d.basefile);
-  localfiles.push(d);
+  var name = file.substr(localdir.length + 1)
+  stat.name = name
+  files[name] = stat
+  localFound++
 });
 
 finder.on('end', function() {
-  console.log('local file list built, %d files found\n', localfiles.length);
-  if (!localfiles.length)
+  console.log('local file list built, %d files found\n', localFound);
+  if (!localFound)
     return done();
-  else if (opts.justdelete)
-    return dodelete();
-  infoqueue.push(localfiles, function() {});
-  headstarted = Date.now();
+  cuttle()
 });
 
-// 2. Process each local file, figure out if we need to put
-// a new version into Manta
-var processed = 0;
-var filestoput = [];
-var errors = [];
-var headstarted;
-var infoqueue = async.queue(processfile, opts.concurrency);
-function processfile(d, cb) {
-  client.info(d.mantafile, function(err, info) {
-    if (err) {
-      processed++;
-      if (err.code === 'NotFoundError') {
-        console.log('%s... not found, adding to put list (%d/%d)',
-            d.mantafile, processed, localfiles.length);
-        filestoput.push(d);
-      } else {
-        var s = util.format('%s... unknown error: %s (%d/%d)',
-            d.mantafile, err.code || err.message, processed, localfiles.length);
-        console.error(s);
-        errors.push(s);
-      }
-      cb();
-      return;
-    }
-
-    if (opts.md5) {
-      // md5
-      var md5sum = crypto.createHash('md5');
-      var rs = fs.createReadStream(d.file);
-      rs.on('error', function(err) {
-        processed++;
-        var s = util.format('%s... read error: %s (%d/%d)',
-            d.mantafile, err.message, processed, localfiles.length);
-        console.error(s);
-        errors.push(s);
-      });
-
-      rs.on('data', md5sum.update.bind(md5sum));
-      rs.on('end', function() {
-        processed++;
-        var md5 = md5sum.digest('hex');
-
-        var remotemd5 = info.md5;
-        if (!remotemd5) {
-          remotemd5 = EMPTY_MD5;
-        } else {
-          remotemd5 = new Buffer(remotemd5, 'base64').toString('hex');
-        }
-
-        if (md5 === remotemd5) {
-          console.log('%s... md5 same as local file, skipping (%d/%d)',
-            d.mantafile, processed, localfiles.length);
-        } else {
-          console.log('%s... md5 is different, adding to put list (%d/%d)',
-            d.mantafile, processed, localfiles.length);
-          filestoput.push(d);
-        }
-
-        cb();
-      });
-      return;
-    } else {
-      processed++;
-      // XXX +info.size is a hack
-      // https//github.com/joyent/node-manta/issues/136
-      if (d.stat.size === +info.size) {
-        console.log('%s... size same as local file, skipping (%d/%d)',
-            d.mantafile, processed, localfiles.length);
-      } else {
-        console.log('%s... size is different, adding to put list (%d/%d)',
-            d.mantafile, processed, localfiles.length);
-        filestoput.push(d);
-      }
-      cb();
-      return;
-    }
-  });
-}
-
-infoqueue.drain = function() {
-  processed = 0;
-  console.log('\nupload list built, %d files staged for uploading (took %dms)\n',
-      filestoput.length, (Date.now() - headstarted) || 0);
-  if (!filestoput.length) {
-    if (opts.delete)
-      dodelete();
-    else
-      done();
-    return;
-  }
-  putqueue.push(filestoput, function() {});
-  putsstarted = Date.now();
-};
-
-// 3. Upload each file that needs to be uploaded, lazily handling
-// directory creation
-var putqueue = async.queue(putfile, opts.concurrency);
-var putsstarted;
-var filesput = 0;
-var filesnotput = 0;
-var bytesput = 0;
-function putfile(d, cb) {
-  if (opts.dryrun) {
-    console.log('%s... uploaded (dryrun)', d.mantafile);
-    return cb();
-  }
-  var rs = fs.createReadStream(d.file);
+function md5file(file, cb) {
+  var rs = fs.createReadStream(localdir + '/' + file.name)
+  var md5sum = crypto.createHash('md5');
+  var called = false
   rs.on('error', function(err) {
-    processed++;
-    var s = util.format('%s... error opening file: %s (%d/%d)',
-      d.mantafile, err.message, processed, filestoput.length);
+    var s = util.format('%s... read error: %s (%d/%d)',
+        remotedir + '/' + file,
+        err.message, localFound.length);
     console.error(s);
     errors.push(s);
-    filesnotput++;
-    cb();
-  });
-
-  rs.on('open', function(fd) {
-    client.put(d.mantafile, rs, {size: d.stat.size, mkdirs: true}, function(err) {
-      processed++;
-      if (err) {
-        var s = util.format('%s... error uploading: %s (%d/%d)',
-          d.mantafile, err.code || err.message, processed, filestoput.length);
-        console.error(s);
-        errors.push(s);
-        filesnotput++;
-      } else {
-        console.log('%s... uploaded (%d/%d)',
-          d.mantafile, processed, filestoput.length);
-        filesput++;
-        bytesput += d.stat.size;
-      }
-      cb();
-    });
-  });
-}
-
-putqueue.drain = function() {
-  processed = 0;
-  console.log('\n%d files (%d bytes) put successfully, %d files failed to put (took %dms)',
-      filesput, bytesput, filesnotput, (Date.now() - putsstarted) || 0);
-  if (opts.delete)
-    dodelete();
-  else
-    done();
-};
-
-// 4. Find all remote files, and delete those that are referenced locally
-var deletequeue = async.queue(deletefile, opts.concurrency);
-var filesdeleted = 0;
-var filesnotdeleted = 0;
-var remotefilestodelete = [];
-var deletesstarted;
-function dodelete() {
-  console.log('\nbuilding remote file list for deletion...');
-  client.ftw(remotedir, {parallel: opts.concurrency}, function(err, res) {
-    if (err) {
-      var e = util.format('error listing remote files: %s', err.code || err.message);
-      console.error('%s\n', e);
-      errors.push(e);
-      done();
-      return;
+    if (!called) {
+      called = true
+      cb(err)
     }
-    res.on('entry', function(d) {
-      if (d.type !== 'object')
-        return;
-      d.mantafile = path.join(d.parent, d.name).replace('/' + process.env.MANTA_USER, '~~');
-
-      var results = localfiles.filter(function(localfile) {
-        return localfile.mantafile === d.mantafile;
-      });
-
-      if (!results.length)
-        remotefilestodelete.push(d);
-    });
-    res.on('end', function() {
-      console.log('remote file list built, %d files found\n', remotefilestodelete.length);
-      if (!remotefilestodelete.length)
-        return done();
-      deletequeue.push(remotefilestodelete, function() {});
-      deletesstarted = Date.now();
-    });
   });
-}
-
-function deletefile(d, cb) {
-  if (opts.dryrun) {
-    console.log('%s... deleted (dryrun)', d.mantafile);
-    return cb();
-  }
-  client.unlink(d.mantafile, function(err) {
-    processed++;
-    if (err) {
-      var s = util.format('%s... error deleting: %s (%d/%d)',
-        d.mantafile, err.code, processed, remotefilestodelete.length);
-      console.error(s);
-      errors.push(s);
-      filesnotdeleted++;
-    } else {
-      console.log('%s... deleted (%d/%d)',
-        d.mantafile, processed, remotefilestodelete.length);
-      filesdeleted++;
+  rs.on('data', md5sum.update.bind(md5sum));
+  rs.on('end', function() {
+    if (!called) {
+      called = true
+      cb(null, md5sum.digest('base64'))
     }
-    cb();
   });
 }
 
-deletequeue.drain = function() {
-  console.log('\n%d files deleted successfully, %d files failed to delete (took %dms)',
-      filesdeleted, filesnotdeleted, (Date.now() - deletesstarted) || 0);
-  done();
-};
+function cuttle() {
+  var syncStart = Date.now()
 
-// 5. Done
-function done() {
-  var ret = 0;
-  if (errors.length) {
-    ret = 1;
-    console.error('\n== errors\n');
-    errors.forEach(function(error) {
-      console.error(error);
-    });
-  }
-  console.log('\ndone');
-  client.close();
-  process.exit(ret);
+  var fish = cuttlefish({
+    //timingDebug: true,
+    // get md5s on demand, if opted in
+    getMd5: opts.md5 ? md5file : null,
+    files: files,
+    path: remotedir,
+    request: getFile,
+    concurrency: opts.concurrency,
+    client: client,
+    delete: opts.delete,
+    onlyDelete: opts.justdelete,
+    dryRun: opts.dryrun
+  });
+
+  var deleted = 0;
+  var sent = 0;
+  var matched = 0;
+  var errors = 0;
+
+  fish.on('error', function(er) {
+    errors++;
+    console.error(er.stack);
+    if (er.file)
+      console.error('File: %s', er.file);
+    if (er.remote)
+      console.error('Remote: %s', er.remote);
+    if (er.task)
+      console.error('Task: %s', er.task);
+  });
+
+  fish.on('task', function(task) {
+    if (task.file)
+      console.log('%s %s...', task.name, task.file);
+  });
+
+  fish.on('file', function(file, status, remote) {
+    console.log('%s %s', file, status)
+  });
+
+  fish.on('delete', function(file) {
+    console.log('deleted %s', file)
+    deleted++;
+  });
+
+  fish.on('match', function() {
+    matched++;
+  });
+
+  fish.on('send', function() {
+    sent++;
+  });
+
+  fish.on('complete', function(results) {
+    console.log('done!');
+    console.log('%d sent, %d matched, %d deleted, %d errors',
+                sent, matched, deleted, errors);
+    console.log('Synced completed in %d ms', Date.now() - syncStart);
+    client.close();
+    var ret = 0;
+    client.close();
+    process.exit(ret);
+  });
 }
 
-// Signals
-process.on('SIGUSR1', function() {
-  if (infoqueue.length) {
-    console.log('%d info tasks waiting to complete', infoqueue.tasks.length);
-    infoqueue.tasks.forEach(function(task) {
-      console.log(task.data.mantafile);
-    });
-  }
-  if (putqueue.length) {
-    console.log('%d put tasks waiting to complete', putqueue.tasks.length);
-    putqueue.tasks.forEach(function(task) {
-      console.log(task.data.mantafile);
-    });
-  }
-  if (deletequeue.length) {
-    console.log('%d delete tasks waiting to complete', deletequeue.tasks.length);
-    deletequeue.tasks.forEach(function(task) {
-      console.log(task.data.mantafile);
-    });
-  }
-});
+function getFile(file, cb) {
+  cb(null, fs.createReadStream(localdir + '/' + file));
+}
